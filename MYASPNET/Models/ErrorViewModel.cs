1967fcf8a9a1771f133f82940ac11ca2427006c8
Yun Jang <jang7j@yahoo.com>
2022-09-19 10:34:05 -0500

    Add project files.

---
+++ MYASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MYASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
